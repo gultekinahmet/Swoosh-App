47b55d1aabb0e8c90d1b6b14745432fbea372f79
Ahmet Gultekin <wolfxanta@yandex.com>
2022-06-28 22:13:54 +0100

    Initial Commit

---
+++ app-swoosh/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Ahmet Gültekin on 25.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
