e0098338cd7c663d229fc779060722c7e5605567
Ahmet Gultekin <wolfxanta@yandex.com>
2022-07-01 22:08:25 +0100

    views auto layout done

--- app-swoosh/ViewController.swift
+++ app-swoosh/ViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var swooshLogo: UIImageView!
+    @IBOutlet var bgImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // every frame has a position, size and origin.
+        /*
+        swooshLogo.frame = CGRect(x: view.frame.size.width / 2 - swooshLogo.frame.size.width / 2 , y: 50, width: swooshLogo.frame.size.width, height: swooshLogo.frame.size.height)
+            */
+        bgImage.frame = view.frame
     }
 
 
