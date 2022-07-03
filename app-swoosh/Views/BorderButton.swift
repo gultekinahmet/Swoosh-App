7c255b3b038abc7c14ca3585b54652c6e532a786
Ahmet Gultekin <wolfxanta@yandex.com>
2022-07-03 15:19:13 +0100

    desired league screen added

--- app-swoosh/Views/BorderButton.swift
+++ app-swoosh/Views/BorderButton.swift
@@ -12,7 +12,8 @@ class BorderButton: UIButton {
     override func awakeFromNib() {
         super.awakeFromNib()
         layer.borderWidth = 3.0
-        layer.borderColor = UIColor.white.cgColor
+        layer.borderColor = UIColor.black.cgColor
+        layer.backgroundColor = UIColor.brown.cgColor
     }
 
 }
