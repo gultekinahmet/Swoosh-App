35dc957701445ec0b5f582e644135a329cdf5889
Ahmet Gultekin <wolfxanta@yandex.com>
2022-07-06 20:37:36 +0100

    IBActions and Model done.

--- app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/Controller/LeagueVC.swift
@@ -8,15 +8,37 @@
 import UIKit
 
 class LeagueVC: UIViewController {
+    
+    var player: Player!
 
     @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
     
+    @IBOutlet var nextBtn: BorderButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        player = Player()
     }
     
-
+    
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
+    
 }
---
+++ app-swoosh/Model/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Ahmet Gültekin on 6.07.2022.
+//
+
+import Foundation
+
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillSevel: String?
+    
+    
+}
